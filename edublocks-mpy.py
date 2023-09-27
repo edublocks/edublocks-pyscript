f68270fa42663ec70628cae4710a5a215528034e
Joshua Lowe <joshualowe1002@gmail.com>
2023-09-27 11:25:35 +0100

    Create edublocks-mpy.py

---
+++ edublocks-mpy.py
@@ -0,0 +1,10 @@
+import time
+from js import prompt
+
+def sleep(seconds):
+	start = now = time.time()
+	while now - start < seconds:
+		now = time.time()
+
+time.sleep = sleep
+__builtins__["input"] = prompt
