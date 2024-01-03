467c39a42f75499581c5e49bac1cf52546856f91
joshualowe1002 <joshualowe1002@gmail.com>
2024-01-03 13:59:10 +0000

    Allow turtle module to be used directly

--- turtle_sk.py
+++ turtle_sk.py
@@ -14,9 +14,13 @@ def get_color(args):
 class Turtle:
     def __init__(self):
         length = len(turtles)
-        self.reference = f"turtle{length+1}"
-        turtles.append(self.reference)
-        window.turtle.execute(f'{self.reference} = Turtle()')
+        if length == 1:
+            self.reference = turtles[0]
+            turtles.append(f"turtle{length+1}")
+        else:
+            self.reference = f"turtle{length+1}"
+            turtles.append(self.reference)
+            window.turtle.execute(f'{self.reference} = Turtle()')
 
     async def forward(self, distance):
         await window.turtle.execute(f'{self.reference}.forward({distance})')
@@ -66,12 +70,64 @@ class Turtle:
     async def shape(self, shape):
         await window.turtle.execute(f'{self.reference}.shape("{shape}")')
 
+    async def write(self, text, move=False, align="left", font=("Arial", 8, "normal")):
+        await window.turtle.execute(f'{self.reference}.write("{text}", move={move}, align="{align}", font=("{font[0]}", {font[1]}, "{font[2]}"))')
+
 class Screen:
     def __init__(self):
         length = len(screens)
-        self.reference = f"screen{length+1}"
-        screens.append(self.reference)
-        window.turtle.execute(f'{self.reference} = Screen()')
+        if length == 1:
+            self.reference = screens[0]
+            turtles.append(f"screen{length+1}")
+        else:
+            self.reference = f"screen{length+1}"
+            screens.append(self.reference)
+            window.turtle.execute(f'{self.reference} = Screen()')
+
         
-    def bgcolor(self, *args):
-        window.turtle.execute(f'{self.reference}.bgcolor("{get_color(args)}")')
+    async def bgcolor(self, *args):
+        await window.turtle.execute(f'{self.reference}.bgcolor("{get_color(args)}")')
+        
+    async def setup(self, w, h):
+        await window.turtle.execute(f'{self.reference}.setup({w}, {h})')
+
+_classes = [
+    "Turtle",
+    "Screen"
+]
+
+_turtle_functions = [
+    "forward",
+    "left",
+    "backward",
+    "right",
+    "goto",
+    "circle",
+    "width",
+    "color",
+    "pencolor",
+    "fillcolor",
+    "begin_fill",
+    "end_fill",
+    "penup",
+    "pendown",
+    "speed",
+    "shape",
+    "write"
+]
+
+_screen_functions = [
+    "bgcolor",
+    "setup"
+]
+
+default_turtle = Turtle()
+default_screen = Screen()
+__all__ = _classes + _turtle_functions + _screen_functions
+
+def _make_global_funcs(functions, cls):
+    for func in functions:
+        globals()[func] = getattr(cls, func)
+
+_make_global_funcs(_turtle_functions, default_turtle)
+_make_global_funcs(_screen_functions, default_screen)
